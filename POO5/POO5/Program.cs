a332e56aa940b409654de0c261c6a3c0552211d6
Rivaldo Junior <155401467+rivaldoclj@users.noreply.github.com>
2025-12-30 23:56:01 -0300

    Add files via upload
    Exercicio 5

---
+++ POO5/POO5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Channels;
+using System.Globalization;
+
+namespace POO5 {
+    class Program {
+        static void Main(string[] args) {
+
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Escreva a seguir os valores de um retângulo: ");
+            Console.Write("Largura: ");
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Altura: ");
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            r.Area();
+            Console.WriteLine("AREA = " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO = " + r.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + r.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ POO5/POO5/Retangulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO5 {
+    class Retangulo {
+
+        public double Largura;
+        public double Altura;
+
+        public double Area() {
+            return Largura * Altura;
+        }
+
+        public double Perimetro() {
+            return (Largura + Altura) * 2.0;
+        }
+
+        public double Diagonal() {
+            return Math.Sqrt((Largura * Largura) + (Altura * Altura));
+        }
+    }
+}
