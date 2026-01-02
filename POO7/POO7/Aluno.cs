99e7cd30845ab65faf189e6153bc1180b7b0755e
Rivaldo Junior <155401467+rivaldoclj@users.noreply.github.com>
2026-01-01 22:00:54 -0300

    Add files via upload
    Exercicio 7

---
+++ POO7/POO7/Aluno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace POO7 {
+    class Aluno {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double NotaFinal() {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public bool Aprovado() {
+            if (NotaFinal() >= 60.0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        public double NotaRestante() {
+            if (Aprovado()) {
+                return 0.0;
+            }
+            else {
+                return 60.0 - NotaFinal();
+            }
+        }
+    }
+}
---
+++ POO7/POO7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace POO7 {
+    class Program {
+        static void Main(string[] args) {
+
+            Aluno a = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            a.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            a.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            a.NotaFinal();
+            Console.WriteLine("NOTA FINAL = " + a.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+            a.Aprovado();
+            a.NotaRestante();
+
+            if (a.Aprovado()) {
+                Console.WriteLine("APROVADO");
+            }
+            else {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine($"FALTARAM {a.NotaRestante().ToString("F2", CultureInfo.InvariantCulture)} PONTOS");
+            }
+        }
+    }
+}
